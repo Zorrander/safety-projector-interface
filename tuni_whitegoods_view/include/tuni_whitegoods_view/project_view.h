539802bfb92e033ebb5f96e6c2d8102830829c65
odin3 <odin3@pop-os.localdomain>
2024-10-25 17:59:22 +0300

    add layer manager

--- tuni_whitegoods_view/include/tuni_whitegoods_view/project_view.h
+++ tuni_whitegoods_view/include/tuni_whitegoods_view/project_view.h
@@ -1,7 +1,10 @@
 #ifndef Projector_H
 #define Projector_H
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <cv_bridge/cv_bridge.h>
+#include <imgui.h>
 #include <std_msgs/Float64MultiArray.h>
 #include <tuni_whitegoods_msgs/Projection.h>
 
@@ -9,7 +12,14 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "tuni_whitegoods_view/view.h"
+
+struct Layer {
+  std::shared_ptr<cv::Mat> mat;
+  bool visible;
+};
 
 class Projector : public View {
  private:
@@ -22,8 +32,16 @@ class Projector : public View {
   std::vector<double> border_homography_array, button_homography_array;
   std::vector<int> projector_resolution;
   ros::Subscriber transform_callback;
-  std::map<std::string, std::shared_ptr<cv::Mat>> layers;
+  std::map<std::string, Layer> layers;
   void transformCallback(const std_msgs::Float64MultiArray::ConstPtr& msg);
+  cv::Mat combined;
+
+  void initializeGLFWandOpenGL();
+  void initializeImGui(GLFWwindow* window);
+  void update_gui();
+  void cleanupImGui();
+  GLuint cvMatToTexture(const cv::Mat& mat);
+  GLFWwindow* window;
 
  public:
   Projector(ros::NodeHandle* nh);
--- tuni_whitegoods_view/src/tuni_whitegoods_view/project_view.cpp
+++ tuni_whitegoods_view/src/tuni_whitegoods_view/project_view.cpp
@@ -15,39 +15,132 @@ Projector::Projector(ros::NodeHandle *nh) {
     ROS_WARN("Parameter 'shiftX' not found, using default value 0.");
   }
   ros::param::get("projector_resolution", projector_resolution);
-  layers["background"] = std::make_shared<cv::Mat>(cv::Mat::zeros(
-      projector_resolution[1], projector_resolution[0], CV_8UC3));
+
+  layers["background"] = {
+      std::make_shared<cv::Mat>(cv::Mat::zeros(
+          projector_resolution[1], projector_resolution[0], CV_8UC3)),
+      true};
+
   cv::namedWindow(OPENCV_WINDOW, cv::WINDOW_NORMAL);
   cv::moveWindow(OPENCV_WINDOW, shift, 0);
   cv::setWindowProperty(OPENCV_WINDOW, cv::WND_PROP_FULLSCREEN,
                         cv::WINDOW_FULLSCREEN);
+
+  // Initialize OpenGL, GLFW, and ImGui
+  initializeGLFWandOpenGL();
+
+  // Create a GLFW window for rendering
+  window = glfwCreateWindow(1280, 720, "Layer Manager", nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+  glewInit();
+
+  // Initialize ImGui
+  initializeImGui(window);
+
   ROS_INFO("ProjectorView running");
 }
 
 Projector::~Projector() { cv::destroyWindow(OPENCV_WINDOW); }
 
 void Projector::init(std::vector<std::shared_ptr<DisplayArea>> zones) {
+  combined = layers["background"].mat->clone();
   for (auto &zone : zones) {
     if (!(zone->name == "projector" || zone->name == "camera")) {
-      cv::Point tl(zone->projector_frame_area[0].x,
-                   zone->projector_frame_area[0].y);
-      cv::Point tr(zone->projector_frame_area[1].x,
-                   zone->projector_frame_area[1].y);
-      cv::Point br(zone->projector_frame_area[2].x,
-                   zone->projector_frame_area[2].y);
-      cv::Point bl(zone->projector_frame_area[3].x,
-                   zone->projector_frame_area[3].y);
-
-      std::vector<cv::Point> rectanglePoints = {tl, tr, br, bl};
-
-      layers[zone->name] = std::make_shared<cv::Mat>(cv::Mat::zeros(
-          projector_resolution[1], projector_resolution[0], CV_8UC3));
-
-      cv::polylines(*layers[zone->name], rectanglePoints, true, cv::Scalar(255),
-                    10, cv::LINE_8);
+      layers[zone->name] = {
+          std::make_shared<cv::Mat>(cv::Mat::zeros(
+              projector_resolution[1], projector_resolution[0], CV_8UC3)),
+          true};
     }
+    // update_gui();
+    // project_image();
   }
-  project_image();
+}
+
+void Projector::initializeGLFWandOpenGL() {
+  if (!glfwInit()) {
+    ROS_ERROR("Failed to initialize GLFW!");
+    exit(EXIT_FAILURE);
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+void Projector::initializeImGui(GLFWwindow *window) {
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 330");
+  ImGui::StyleColorsDark();
+}
+
+void Projector::cleanupImGui() {
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+}
+
+GLuint Projector::cvMatToTexture(const cv::Mat &mat) {
+  GLuint textureID;
+  glGenTextures(1, &textureID);
+  glBindTexture(GL_TEXTURE_2D, textureID);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  cv::Mat rgb;
+  if (mat.channels() == 3)
+    cv::cvtColor(mat, rgb, cv::COLOR_BGR2RGB);
+  else
+    rgb = mat;
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, rgb.cols, rgb.rows, 0, GL_RGB,
+               GL_UNSIGNED_BYTE, rgb.data);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  return textureID;
+}
+
+void Projector::update_gui() {
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
+
+  ImGui::Begin("Layer Manager and Combined Image");
+
+  ImGui::Columns(2, "SideBySideColumns", false);
+
+  GLuint textureID = cvMatToTexture(combined);
+
+  ImGui::BeginGroup();
+  ImGui::Text("Combined Image");
+  ImGui::Image((ImTextureID)(uintptr_t)textureID, ImVec2(1024, 768));
+  ImGui::EndGroup();
+
+  ImGui::NextColumn();
+
+  ImGui::BeginGroup();
+  ImGui::Text("Layer Manager");
+
+  for (auto it = layers.begin(); it != layers.end(); ++it) {
+    const std::string &name = it->first;
+    Layer &layer = it->second;
+
+    ImGui::Checkbox(name.c_str(), &layer.visible);
+    ImGui::SameLine();
+  }
+
+  ImGui::EndGroup();
+
+  ImGui::Columns(1);
+  ImGui::End();
+  // Render ImGui
+  ImGui::Render();
+  glClear(GL_COLOR_BUFFER_BIT);
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+  // Swap OpenGL buffers
+  glfwSwapBuffers(window);
+  glfwPollEvents();
 }
 
 void Projector::updateButtons(
@@ -92,33 +185,37 @@ void Projector::updateDisplayAreas(
 
       const cv::Scalar color(255, 255, 255);
       const int thickness = 10;
+      bool visibility = layers[zone->name].visible;
 
-      layers[zone->name] = std::make_shared<cv::Mat>(cv::Mat::zeros(
-          projector_resolution[1], projector_resolution[0], CV_8UC3));
+      layers[zone->name] = {
+          std::make_shared<cv::Mat>(cv::Mat::zeros(
+              projector_resolution[1], projector_resolution[0], CV_8UC3)),
+          visibility};
 
-      cv::polylines(*layers[zone->name], rectanglePoints, true, cv::Scalar(255),
-                    10, cv::LINE_8);
+      cv::polylines(*layers[zone->name].mat, rectanglePoints, true,
+                    cv::Scalar(255), 10, cv::LINE_8);
 
       std::vector<std::shared_ptr<Button>> buttons;
       zone->fetchButtons(buttons);
-      updateButtons(buttons, layers[zone->name]);
+      updateButtons(buttons, layers[zone->name].mat);
 
       std::vector<std::shared_ptr<StaticBorder>> borders;
       zone->fetchBorders(borders);
-      updateBorders(borders, layers[zone->name]);
+      updateBorders(borders, layers[zone->name].mat);
     }
   }
+  combined = layers["background"].mat->clone();
+  for (auto it = layers.begin(); it != layers.end(); ++it) {
+    if (it->second.visible) {
+      cv::bitwise_or(combined, *(it->second.mat), combined);
+    }
+  }
+
+  update_gui();
   project_image();
 }
 
 void Projector::project_image() {
-  cv::Mat combined = layers["background"]->clone();
-
-  for (auto it = layers.begin(); it != layers.end(); ++it) {
-    const std::string &name = it->first;
-    std::shared_ptr<cv::Mat> &layer = it->second;
-    cv::bitwise_or(combined, *layer, combined);
-  }
   cv::imshow(OPENCV_WINDOW, combined);
-  cv::waitKey(100);
+  cv::waitKey(1);
 }
